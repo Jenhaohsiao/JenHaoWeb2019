5017652e028ea0340056e527b253be52b1bf22b2
Jenhao Hsiao <jenhao.hsiao@gmail.com>
2019-05-04 11:31:02 -0400

    Added scrolling

--- SRC/JS/webJs.js
+++ SRC/JS/webJs.js
@@ -9,7 +9,7 @@ console.log("shown Height is: ", shownHeight);
 
 
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     var pageY = window.pageYOffset;
     console.clear();
     console.log("Site height is: ", webSiteHeight);
@@ -18,3 +18,36 @@ window.addEventListener("scroll", function() {
     console.log("pageY: ", pageY);
 
 })
+
+
+//=========================== scrollspy
+
+$(document).ready(function () {
+    // Add scrollspy to <body>
+    $('body').scrollspy({
+        target: ".navbar",
+        offset: 50
+    });
+
+    // Add smooth scrolling on all links inside the navbar
+    $("#nav-bar a").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
