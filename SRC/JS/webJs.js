66a7271f8adf82730df41f4631f0a4a8b12f9a8a
Jenhao Hsiao <jenhao.hsiao@gmail.com>
2019-05-05 00:28:16 -0400

    New UI

--- SRC/JS/webJs.js
+++ SRC/JS/webJs.js
@@ -1,4 +1,4 @@
-var webSiteHeight = document.body.clientHeight;
+var webSiteHeight = document.documentElement.clientHeight
 var windowsHeight = window.innerHeight;
 var shownHeight = webSiteHeight - windowsHeight;
 
@@ -20,34 +20,15 @@ window.addEventListener("scroll", function () {
 })
 
 
-//=========================== scrollspy
+function scrollToPoint(_elementID) {
 
-$(document).ready(function () {
-    // Add scrollspy to <body>
-    $('body').scrollspy({
-        target: ".navbar",
-        offset: 50
+
+    var elementPoint = document.getElementById(_elementID).offsetTop;
+    var contectSection = document.getElementById('contect-section');
+    console.log("elementPoint:", elementPoint);
+
+    contectSection.scroll({
+        top: elementPoint,
+        behavior: "smooth"
     });
-
-    // Add smooth scrolling on all links inside the navbar
-    $("#nav-bar a").on('click', function (event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-    });
-});
+}
